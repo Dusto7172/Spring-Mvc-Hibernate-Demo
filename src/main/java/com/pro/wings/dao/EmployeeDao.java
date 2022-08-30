4530fb1b944c4a9b9de5f0a5827b36ded3275745
Dusto7172 <rockyrock7125@gmail.com>
2022-08-30 07:49:15 +0530

    Spring-Mvc-Hibernate-Demo

---
+++ src/main/java/com/pro/wings/dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.pro.wings.dao;
+
+import com.pro.wings.entity.Employee;
+
+public interface EmployeeDao {
+
+	public boolean addEmployee(Employee entity);
+
+}
---
+++ src/main/java/com/pro/wings/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.pro.wings.service;
+
+import com.pro.wings.entity.Employee;
+
+public interface EmployeeService {
+
+	public boolean addEmployee(Employee emp);
+}
